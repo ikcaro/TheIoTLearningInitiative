b7580b5bdc2b03af41d44799484fc821c6f84f59
Angel Perez <angellrperezz@gmail.com>
2016-07-14 06:19:14 +0000

    Internet of Things 101: Challenges
    Data Reporting
    - System Data, Device ID
    - System Communications Protocols
      - Device ID
      - RESTFull API
    - Cloud Computing Services
      - freeboard.io, dweet.io
    - Cloud Computing Platforms
      - IBM Watson IoT Platform
    
    ToDo: Seeed Studio Grove Kit
    
    Signed-off-by: Angel Perez <angellrperezz@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 
 import paho.mqtt.client as paho
+import json
+import uuid
 import psutil
 import pywapi
 import signal
 import sys
 import time
+import dweepy
 
 from threading import Thread
+from flask import Flask
+from flask_restful import Api, Resource
 
-def functionApiWeather():
-    data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
-    message = data['location']['name']
-    message = message + ", Temperature " + data['current_conditions']['temperature'] + " C"
-    message = message + ", Atmospheric Pressure " + data['current_conditions']['barometer']['reading'][:-3] + " mbar"
-    return message
+DeviceID = 0
+
+class DataSensorRestApi(Resource):
+    def get(self):
+        netdata = psutil.net_io_counters()
+        data = netdata.packets_sent + netdata.packets_recv
+        return data
 
 def functionDataActuator(status):
     print "Data Actuator Status %s" % status
@@ -24,49 +30,108 @@ def functionDataActuatorMqttOnMessage(mosq, obj, msg):
     functionDataActuator(msg.payload)
 
 def functionDataActuatorMqttSubscribe():
+    global DeviceID
+    
     mqttclient = paho.Client()
     mqttclient.on_message = functionDataActuatorMqttOnMessage
     mqttclient.connect("test.mosquitto.org", 1883, 60)
-    mqttclient.subscribe("IoT101/DataActuator", 0)
+    mqttclient.subscribe("IoT101/" + DeviceID + "/DataActuator", 0)
     while mqttclient.loop() == 0:
         pass
 
 def functionDataSensor():
-    netdata = psutil.net_io_counters()
-    data = netdata.packets_sent + netdata.packets_recv
-    return data
+    s = DataSensorRestApi()
+    return s.get()
 
 def functionDataSensorMqttOnPublish(mosq, obj, msg):
     print "Data Sensor Mqtt Published!"
 
 def functionDataSensorMqttPublish():
+    global DeviceID
+    
+    topic = "IoT101/" + DeviceID + "/DataSensor"
     mqttclient = paho.Client()
     mqttclient.on_publish = functionDataSensorMqttOnPublish
     mqttclient.connect("test.mosquitto.org", 1883, 60)
     while True:
         data = functionDataSensor()
-        topic = "IoT101/DataSensor"
         mqttclient.publish(topic, data)
-        time.sleep(1)
+        time.sleep(5)
+
+def functionApiWeather():
+    while True:
+        data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
+        message = data['location']['name']
+        message = message + ", Temperature " + data['current_conditions']['temperature'] + " C"
+        message = message + ", Atmospheric Pressure " + data['current_conditions']['barometer']['reading'][:-3] + " mbar"
+        
+        print "API Weather: %s" % message
+        time.sleep(5)
+
+def functionServicesDweet():
+    s = DataSensorRestApi()
+    
+    while True:
+        dweepy.dweet_for('NetworkStatsIoT', {'Packets': s.get()})
+        print dweepy.get_latest_dweet_for('NetworkStatsIoT')
+        time.sleep(5)
+
+def functionDataSensorMqttPublishIBM():
+    global DeviceID
+    
+    organization = "quickstart"
+    deviceType = "iotsample-gateway"
+    topic = "iot-2/evt/status/fmt/json"
+    username = ""
+    password = ""
+    
+    clientID = "d:" + organization + ":" + deviceType + ":" + DeviceID
+    broker = organization + ".messaging.internetofthings.ibmcloud.com"
+    
+    mqttclient = paho.Client(clientID)
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect(broker, 1883, 60)
+    
+    while True:
+        data = functionDataSensor()
+        msg = json.JSONEncoder().encode({"d":{"netpackets":data}})
+        mqttclient.publish(topic, msg)
+        time.sleep(5)
+
+def functionDataSensorFlask():
+    app = Flask(__name__)
+    api = Api(app)
+    
+    api.add_resource(DataSensorRestApi, '/sensor')
+    app.run(host='0.0.0.0', debug=True)
 
 def functionSignalHandler(signal, frame):
     sys.exit(0)
 
 if __name__ == '__main__':
+    DeviceID = hex(uuid.getnode())[2:-1]
+    DeviceID = format(long(DeviceID, 16),'012x')
+    
+    print "Hello Internet of Things 101"
+    print "Device ID: %s" % DeviceID
 
-    signal.signal(signal.SIGINT, functionSignalHandler)
+    signal.signal(signal.SIGINT,  functionSignalHandler)
 
     threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
     threadmqttpublish.start()
 
     threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
     threadmqttsubscribe.start()
+    
+    threaddataweather = Thread(target=functionApiWeather)
+    threaddataweather.start()
+    
+    threaddweet = Thread(target=functionServicesDweet)
+    threaddweet.start()
+    
+    threadmqttpublishibm = Thread(target=functionDataSensorMqttPublishIBM)
+    threadmqttpublishibm.start()
 
-    while True:
-        print "Hello Internet of Things 101"
-        print "Data Sensor: %s " % functionDataSensor()
-        print "API Weather: %s " % functionApiWeather()
-        time.sleep(5)
+    functionDataSensorFlask()
 
 # End of File
-
